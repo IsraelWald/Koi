d6fca9559cc8cd0fc426a5e3b1b5110d85fafdea
Israel Waldner <isaelwaldner@g.horrycountyschools.net>
2022-11-09 14:10:27 -0500

    Added function declaration class

---
+++ src/koi/koi_function.py
@@ -0,0 +1,26 @@
+from .environment import Environment
+from .koi_callable import KoiCallable
+from .stmt import Function
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .interpreter import Interpreter
+
+
+class KoiFunction(KoiCallable):
+    def __init__(self, declaration: Function) -> None:
+        self.decl = declaration
+    
+    def call(self, interpreter: Interpreter, args: List):
+        env = Environment(interpreter.globals)
+        for idx, param in enumerate(self.decl.params):
+            env.define(param.lexeme, args[idx])
+
+        interpreter._exec_block(self.decl.body, env)
+        return None
+
+    def arity(self) -> int:
+        return len(self.decl.params)
+
+    def __repr__(self) -> str:
+        return f"<function {self.decl.name.lexeme}>"
