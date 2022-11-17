4d825e5bcae3e99182fe2c67e38ce9a9ac5d32d6
mordy <imky171@gmail.com>
2022-11-16 20:43:53 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Koi",
-    version="1.1.1",
+    version="2.0.0",
     description="Koi Language",
     author="Israel Waldner",
     author_email="imky171@gmail.com",
