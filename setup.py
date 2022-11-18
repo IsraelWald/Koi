b0d02700279ce5e4fceca6b00fe786224fc8d72f
mordy <imky171@gmail.com>
2022-11-17 19:26:41 -0500

    Added version number for twine

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="Koi",
-    version="2.0.0",
-    description="Koi Language",
+    name="Koi Language",
+    version="1.0.0",
+    description="Koi Programming Language",
     author="Israel Waldner",
     author_email="imky171@gmail.com",
     packages=["src", "src.koi", "src.koi.std"],
