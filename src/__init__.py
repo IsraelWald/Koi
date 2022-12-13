bd03ed25e1a942b847f0cba3c2dda44a384ae201
Israel Waldner <60708054+mordy-python@users.noreply.github.com>
2022-12-13 18:39:45 +0000

    Renamed token.py to tokens.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .koi.scanner import Scanner
 from .koi.token_type import TokenType
-from .koi.token import Token
+from .koi.tokens import Token
 from .koi.parser import Parser
 from .koi.ast_printer import AstPrinter
 from .koi.koi import Koi
